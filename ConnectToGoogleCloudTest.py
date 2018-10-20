82a29dbae86fe010eb95d39ee2abdcd8a75ffd9f
Jordan Cowen <jkcowen@iastate.edu>
2018-10-20 01:06:17 -0500

    Added to gitignore

--- ConnectToGoogleCloudTest.py
+++ ConnectToGoogleCloudTest.py
@@ -1,5 +1,6 @@
 # Imports the Google Cloud client library
 from google.cloud import datastore
+import json
 
 # Instantiates a client
 datastore_client = datastore.Client()
@@ -14,8 +15,13 @@ task_key = datastore_client.key(kind, name)
 # Prepares the new entity
 task = datastore.Entity(key=task_key)
 task['description'] = 'Buy milk'
+task['timeToComplete'] = 45
 
 # Saves the entity
 datastore_client.put(task)
+
+task2 = datastore_client.get(task_key)
+
+print task2
 
 print('Saved {}: {}'.format(task.key.name, task['description']))
